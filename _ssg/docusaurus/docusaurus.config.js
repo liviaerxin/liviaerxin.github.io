24b4e3556982b68e2e263c11639a8778e48c1549
liviaerxin <1yue8haogaoqi@gmail.com>
2023-05-29 23:58:58 +0800

    chore: fix bug

--- _ssg/docusaurus/docusaurus.config.js
+++ _ssg/docusaurus/docusaurus.config.js
@@ -11,18 +11,19 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://liviaerxin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'liviaerxin', // Usually your GitHub org/user name.
+  projectName: 'liviaerxin.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
