5617bfac9e4d1fef73f35d2c62d0f7dbe2b67fc3
liviaerxin <1yue8haogaoqi@gmail.com>
2023-07-16 01:59:24 +0800

    xxx

---
+++ _ssg/docusaurus/src/theme/BlogSidebar/Desktop/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import {translate} from '@docusaurus/Translate';
+import type {Props} from '@theme/BlogSidebar/Desktop';
+
+import styles from './styles.module.css';
+
+import {useHideableNavbar} from '@docusaurus/theme-common/internal';
+
+export default function BlogSidebarDesktop({sidebar}: Props): JSX.Element {
+  const hideOnScroll = true;
+  const {navbarRef, isNavbarVisible} = useHideableNavbar(hideOnScroll);
+
+  return (
+    <aside className="col col--3">
+      <nav
+        ref={navbarRef}
+        className={clsx(
+          styles.sidebar,
+          hideOnScroll && [
+            styles.navbarHideable,
+            !isNavbarVisible && styles.navbarHidden,
+          ],
+          'thin-scrollbar',
+        )}
+        aria-label={translate({
+          id: 'theme.blog.sidebar.navAriaLabel',
+          message: 'Blog recent posts navigation',
+          description: 'The ARIA label for recent posts in the blog sidebar',
+        })}>
+        <div className={clsx(styles.sidebarItemTitle, 'margin-bottom--md')}>
+          {sidebar.title}
+        </div>
+        <ul className={clsx(styles.sidebarItemList, 'clean-list')}>
+          {sidebar.items.map((item) => (
+            <li key={item.permalink} className={styles.sidebarItem}>
+              <Link
+                isNavLink
+                to={item.permalink}
+                className={styles.sidebarItemLink}
+                activeClassName={styles.sidebarItemLinkActive}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
