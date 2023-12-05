726b859bb64d99295eaef0cf3464c829d9f42fd4
frank chen <frankchen@astri.org>
2023-12-05 13:44:18 +0000

    update 6 files and create 3 files

--- code-snippets/socket_tls.py
+++ code-snippets/socket_tls.py
@@ -41,3 +41,26 @@ def server():
 
 if __name__ == "__main__":
     ca_cert = "/workspaces/liviaerxin.github.io/attachments/cert/RootCA.crt"
+    server_cert = "/workspaces/liviaerxin.github.io/attachments/cert/localhost.crt"
+    import socket
+    import ssl
+
+    # Client setup
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8888))
+
+    # TLS setup
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.load_verify_locations(ca_cert)  # Assuming a self-signed certificate
+
+    # Wrap the connection
+    ssl_connection = context.wrap_socket(client_socket, server_hostname='localhost')
+
+    # Send and receive data
+    ssl_connection.send("Hello from the client!".encode())
+    data = ssl_connection.recv(1024)
+    print("Received:", data.decode())
+
+    # Close the connection
+    ssl_connection.close()
+    client_socket.close()
