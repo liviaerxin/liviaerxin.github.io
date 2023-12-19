53992201853292fd82dec2892c35ab18d1460b6b
frank chen <frankchen@astri.org>
2023-12-19 08:35:40 +0000

    update 2 files and create 1 file

--- code-snippets/object_reflection.py
+++ code-snippets/object_reflection.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3.10
+
 import ctypes
 
 assert ctypes.cast(id(42), ctypes.py_object).value == 42
 
-
+# It's recommended to go to see [python 3.10 unicodeobject.h](https://github.com/python/cpython/blob/3.10/Include/cpython/unicodeobject.h#L85-L244)
 class PyASCIIObject(ctypes.Structure):
     # internal fields of the string object
     _fields_ = [
@@ -16,38 +18,90 @@ class PyASCIIObject(ctypes.Structure):
         ("ascii", ctypes.c_uint, 1),
         ("ready", ctypes.c_uint, 1),
         ("_padding", ctypes.c_uint, 24),
-        ("wstr", ctypes.c_wchar_p),
+        ("wstr", ctypes.POINTER(ctypes.c_wchar))
     ]
 
-
-class PyCompactUnicodeObject(ctypes.Structure):
+    def __repr__(self) -> str:
+        return f"ob_refcnt[{self.ob_refcnt}], length[{self.length}], interned[{self.interned}], kind[{self.kind}], compact[{self.compact}], ascii[{self.ascii}], ready[{self.ready}]"
+    
+class PyCompactUnicodeObject(PyASCIIObject):
+    # internal fields of the string object
     _fields_ = [
-        ("_base", PyASCIIObject),
         ("utf8_length", ctypes.c_ssize_t),
-        ("utf8", ctypes.c_void_p),
+        ("utf8", ctypes.POINTER(ctypes.c_char)),
         ("wstr_length", ctypes.c_ssize_t),
     ]
 
-
-class _Data(ctypes.Union):
+    def __repr__(self) -> str:
+        return super().__repr__() + f" utf8_length[{self.utf8_length}], utf8[{self.utf8}], wstr_length[{self.wstr_length}]"
+    
+class PyUnicodeObject(PyCompactUnicodeObject):
+    class _Data(ctypes.Union):
+        _fields_ = [
+            ("any", ctypes.c_void_p),
+            ("latin1", ctypes.POINTER(ctypes.c_uint8)),
+            ("ucs2", ctypes.POINTER(ctypes.c_uint16)),
+            ("ucs4", ctypes.POINTER(ctypes.c_uint32)),
+        ]
+    
     _fields_ = [
-        ("any", ctypes.c_void_p),
-        ("latin1", ctypes.c_char_p),
-        ("ucs2", ctypes.c_char_p),
-        ("ucs4", ctypes.c_char_p),
+        ("data", _Data),
     ]
-
-
-class PyUnicodeObject(ctypes.Structure):
-    _fields_ = [("_base", PyCompactUnicodeObject), ("data", _Data)]
-
-
-# assert ctypes.cast(PyUnicodeObject.from_address(id("Hello")), PyASCIIObject).kind == 1
+    
 # assert PyUnicodeObject.from_address(id("Hello")).kind == 1
 # assert PyUnicodeObject.from_address(id("你好")).kind == 2
 # assert PyUnicodeObject.from_address(id("🤨")).kind == 4
 
-s = "hello world"
-su = PyUnicodeObject.from_address(id(s))
+# keep it from GC
+# 1. compact ascii: kind[1], compact[1], ascii[1], ready[1]
+print(f"\n -compact ascii:")
+string_obj = "Hello, ctypes!"
+addr = id(string_obj)
+ascii_obj = PyASCIIObject.from_address(addr)
+print(ascii_obj)
 
-# ctypes.string_at(su.data)
+# compact ascii: data starts just after the structure
+data_addr = addr + ctypes.sizeof(PyASCIIObject)
+data = ctypes.cast(data_addr, ctypes.c_char_p)
+print(f"data: {data.value}")
+
+
+# 2. compact: kind[2], compact[1], ascii[0], ready[1]
+print(f"\n -compact:")
+string_obj = "你好!"
+addr = id(string_obj)
+ascii_obj = PyASCIIObject.from_address(addr)
+print(ascii_obj)
+
+compact_obj = PyCompactUnicodeObject.from_address(addr)
+print(compact_obj)
+
+# compact: data starts just after the structure
+data_addr = addr + ctypes.sizeof(PyCompactUnicodeObject)
+data = ctypes.cast(data_addr, ctypes.POINTER(ctypes.c_uint16))
+print(f"data: {data[0]}, {data[0]:#06x}, {chr(data[0])}")
+print(f"data: {data[1]}, {data[1]:#06x}, {chr(data[1])}")
+print(f"data: {data[2]}, {data[2]:#06x}, {chr(data[2])}")
+print(f"data: {data[3]}, {data[3]:#06x}, {chr(data[3])}")
+
+
+# 3. compact: kind[4], compact[1], ascii[0], ready[1]
+print(f"\n -compact:")
+string_obj = "你好🤨"
+addr = id(string_obj)
+ascii_obj = PyASCIIObject.from_address(addr)
+print(ascii_obj)
+
+compact_obj = PyCompactUnicodeObject.from_address(addr)
+print(compact_obj)
+
+# compact: data starts just after the structure
+data_addr = addr + ctypes.sizeof(PyCompactUnicodeObject)
+data = ctypes.cast(data_addr, ctypes.POINTER(ctypes.c_uint32))
+print(f"data: {data[0]}, {data[0]:#010x}, {chr(data[0])}")
+print(f"data: {data[1]}, {data[1]:#010x}, {chr(data[1])}")
+print(f"data: {data[2]}, {data[2]:#010x}, {chr(data[2])}")
+print(f"data: {data[3]}, {data[3]:#010x}, {chr(data[3])}")
+
+# 4. legacy string: kind[2], compact[0], ascii[0] is deprecated
+# I can't produce it in Python3.10, maybe you can try python2.7
