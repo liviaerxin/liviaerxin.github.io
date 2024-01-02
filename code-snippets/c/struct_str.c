f12a65c666cbd824978c0e0a784e8aa81ff88ed2
frank chen <frankchen@astri.org>
2024-01-02 07:24:49 +0000

    update 4 files and create 1 file

--- code-snippets/c/struct_str.c
+++ code-snippets/c/struct_str.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 typedef struct
 {
     size_t length;
-    char data[1]; // size is same as the size of a address, 8 bytes in 64-bits system.
+    char data[1]; // flexible array member, NULL terminated in last element
 } String;
 
-String *string_new(const char *str)
+/* Using String_SIZE instead of sizeof(String) saves 7 bytes per string in 64 bit machine
+ */
+#define String_SIZE (offsetof(String, data) + 1)
+
+String *string_new_with_size(const char *str, size_t len)
 {
-    size_t len = strlen(str);
-    String *s = malloc(sizeof(String) + len);
+    String *s = malloc(String_SIZE + len);
 
     if (s == NULL)
     {
@@ -25,6 +29,12 @@ String *string_new(const char *str)
     return s;
 }
 
+String *string_new(const char *str)
+{
+    size_t len = strlen(str);
+    return string_new_with_size(str, len);
+}
+
 void string_free(String *s)
 {
     free(s);
@@ -33,6 +43,8 @@ void string_free(String *s)
 int main()
 {
     printf("Struct String Size: %zu\n", sizeof(String));
+    printf("Struct String Size: %zu\n", String_SIZE);
+
     String *s = string_new("Hello world!");
 
     if (s != NULL)
@@ -41,6 +53,30 @@ int main()
         printf("Data: %s\n", s->data);
 
         string_free(s);
+    }
+
+    // Array of pointers to String struct, don't use an array of String struct as it allocate fixed memory
+    printf("\nArray of pointers to String Struct \n");
+    int array_size = 3;
+    String *myArray[array_size];
+
+    for (int i = 0; i < array_size; ++i)
+    {
+        int size_for_w = i + 10; // Vary the size of the flexible array
+        myArray[i] = string_new_with_size("Hello world! Hello world! Hello world!", size_for_w);
+    }
+
+    // Access and print elements
+    for (int i = 0; i < array_size; ++i) {
+        printf("Element %d:\n", i);
+        printf("Length: %zu\n", myArray[i]->length);
+        printf("  Data: %s\n", myArray[i]->data);
+
+    }
+
+    // Free allocated memory
+    for (int i = 0; i < array_size; ++i) {
+        free(myArray[i]);
     }
 
     return 0;
--- code-snippets/python/c-types/BytesObject.py
+++ code-snippets/python/c-types/BytesObject.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.10
 
 import ctypes
-
+import sys
 
 # [bytesobject.c](https://github.com/python/cpython/blob/3.10/Objects/bytesobject.c)
 # [bytesobject.h](https://github.com/python/cpython/blob/3.10/Include/cpython/bytesobject.h#L5-L15)
@@ -17,23 +17,29 @@ class PyVarObject(ctypes.Structure):
 
 class PyBytesObject(PyVarObject):
     _fields_ = [
-        ("hash", ctypes.c_ssize_t),
-        # ("ob_sval", ctypes.c_char),  # 1 byte
+        ("hash", ctypes.c_ssize_t), # 8 bytes in 64 bit
+        ("ob_sval", ctypes.c_char),  # 1 byte
     ]
 
     def __repr__(self) -> str:
         return f"ob_refcnt[{self.ob_refcnt}], ob_type[{self.ob_type}], ob_size[{self.ob_size}], hash[{self.hash}]"
 
 
-print(ctypes.sizeof(PyVarObject))
+print(ctypes.sizeof(PyBytesObject))
 
-bytes_obj = b"hello world!"
+bytes_obj = b"hello world adadd!"
+print(f"sizeof bytes_obj: {sys.getsizeof(bytes_obj)}")
+
 addr = id(bytes_obj)
+
+# Get ctypes object from memory address which points to C data structure
 py_bytes_obj = PyBytesObject.from_address(addr)
 print(py_bytes_obj)
 
-data_addr = addr + ctypes.sizeof(PyBytesObject)
+# PyBytesObject save the bytes in a flexible array member `ob_sval[1]`
+data_addr = addr + PyBytesObject.ob_sval.offset
 data = ctypes.cast(data_addr, ctypes.c_char_p)
 print(f"data: {data.value}")
-
+# Or
 print(f"data: {ctypes.string_at(data_addr, py_bytes_obj.ob_size)}")
+print(f"total size in PyBytesObject struct: {PyBytesObject.ob_sval.offset + py_bytes_obj.ob_size + 1}")
